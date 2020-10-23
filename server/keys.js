d94b0fcbc1f1795b9eb3830eaecf503566f8c2de
rishav <myemail>
2020-10-23 19:23:41 +0530

    updates

--- server/keys.js
+++ server/keys.js
@@ -1,11 +1,13 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     mongoURI: process.env.MONGO_URI,
+    redirectDomain: process.env.REDIRECT_DOMAIN,
   };
 } else {
   module.exports = {
     mongoURI:
       "mongodb+srv://phoneData:11700946@LpU@cluster0.rjbtr.mongodb.net/mobiles?retryWrites=true&w=majority",
+    redirectDomain: "http://localhost:3001",
   };
 }
 //   mongoURI:'mongodb+srv://phoneData:11700946@LpU@cluster0.rjbtr.mongodb.net/mobiles?retryWrites=true&w=majority',
